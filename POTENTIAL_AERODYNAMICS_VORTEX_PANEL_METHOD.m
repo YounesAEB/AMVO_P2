172a823a5cb4d34ff95242eb1e222538f82a92a6
YounesAEB <109099577+YounesAEB@users.noreply.github.com>
2023-10-23 19:29:07 +0200

    Update POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m

--- POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m
+++ POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m
@@ -8,9 +8,11 @@ clc; clear; close all;
 
 % Input parameters
 
-N = 80; % Number of panels
+N = 200; % Number of panels
 R = 1; % Radius of the cilinder
-Qinf = [30;0]; % Freestream Velocity field
+AoA = 6; % Angle of attack
+Uinf = 30;
+Qinf = Uinf*[cosd(AoA);sind(AoA)]; % Freestream Velocity field
 
 %% GEOMETRIC PREPROCESS - THIS CAN ALL BE A FUNCTION!
 % Coordinate definition and calculation
@@ -63,7 +65,7 @@ hold off
 axis equal
 
 %% POTENTIAL AERODYNAMICS - VELOCITY AND PRESSURE FIELDS CALCULATION
-%%{
+%{
 % CONSTANT STRENGHT SOURCE DISTRIBUTION - THIS CAN ALL BE A FUNCTION! 
 % (Used for symmetric airfoils with zero angle of attack)
 % Variable Definition
@@ -82,6 +84,10 @@ for i = 1:N
     for j = 1:N
         if i == j
             A(i,i) = 0.5;
+            uIndLoc(i,i) = 0;
+            wIndLoc(i,i) = 0.5;
+            uInd(i,i) = uIndLoc(i,j)*cj(j) + wIndLoc(i,j)*sj(j);
+            wInd(i,i) = -uIndLoc(i,j)*sj(j) + wIndLoc(i,j)*cj(j);
         else
             xLoc(i,j) = (coord_xC(i,1)-coord_xP(j,1))*cj(j)...
                       - (coord_xC(i,2)-coord_xP(j,2))*sj(j);
@@ -99,14 +105,14 @@ for i = 1:N
 
             theta2 = atan2(zLoc(i,j),xLoc(i,j)-lp(j));
 
-            uIndLoc(i,j) = (1/4*pi)*log((r1^2)/(r2^2));
+            uIndLoc(i,j) = (1/(4*pi))*log((r1^2)/(r2^2));
             wIndLoc(i,j) = (theta2 - theta1)/(2*pi);
             
             % Change of the velocity components to the global frame
             uInd(i,j) = uIndLoc(i,j)*cj(j) + wIndLoc(i,j)*sj(j);
             wInd(i,j) = -uIndLoc(i,j)*sj(j) + wIndLoc(i,j)*cj(j);
 
-            A(i,j) = [uInd(i,j) wInd(i,j)]*Ncj(i,:)';
+            A(i,j) = [uInd(i,j),wInd(i,j)]*Ncj(i,:)';
         end        
     end
 end
@@ -114,7 +120,7 @@ end
 % Source strenghts obtention 
 gamma = A\b; % sigma in reality, stated gamma to use the same post process code
 %}
-%{
+%%{
 % CONSTANT STRENGHT VORTEX DISTRIBUTION - THIS CAN ALL BE A FUNCTION! 
 % (Used for asymmetric or symmetric airfoils with non-zero angle of attack)
 % Variable Definition
@@ -133,6 +139,10 @@ for i = 1:N
     for j = 1:N
         if i == j
             A(i,i) = -0.5;
+            uIndLoc(i,i) = 0.5;
+            wIndLoc(i,i) = 0;
+            uInd(i,i) = uIndLoc(i,j)*cj(j) + wIndLoc(i,j)*sj(j);
+            wInd(i,i) = -uIndLoc(i,j)*sj(j) + wIndLoc(i,j)*cj(j);
         else
             xLoc(i,j) = (coord_xC(i,1)-coord_xP(j,1))*cj(j)...
                       - (coord_xC(i,2)-coord_xP(j,2))*sj(j);
@@ -151,7 +161,7 @@ for i = 1:N
             theta2 = atan2(zLoc(i,j),xLoc(i,j)-lp(j));
 
             uIndLoc(i,j) = (theta2 - theta1)/(2*pi);
-            wIndLoc(i,j) = (1/4*pi)*log((r2^2)/(r1^2));
+            wIndLoc(i,j) = (1/(4*pi))*log((r2^2)/(r1^2));
             
             % Change of the velocity components to the global frame
             uInd(i,j) = uIndLoc(i,j)*cj(j) + wIndLoc(i,j)*sj(j);
@@ -168,21 +178,42 @@ A(N/4,end) = 1;
 b(N/4)     = 0;
 % Sorce strenghts obtention 
 gamma = A\b;
+gamma(N/4) = (gamma(N/4-1)+gamma(N/4+1))/2;
 %}
 %% POSTPROCESSING
 %%{
 % Control point velocity computation: Vi = Qinf + sum_j=1^N(sig_j*V^~_i,j)
+% V = zeros(N,2);
+% for i=1:N
+% sum_x = 0;
+% sum_z = 0;
+%     for j=1:N
+%         sum_x = sum_x + gamma(j)*uInd(i,j);
+%         sum_z = sum_z + gamma(j)*wInd(i,j);
+%     end
+% V(i,1) = Qinf(1,1) + sum_x;
+% V(i,2) = Qinf(2,1) + sum_z;
+% end
+
 V = zeros(N,2);
 for i=1:N
-sum_i = 0;
-sum_j = 0;
+sum = zeros(2,1);
+
     for j=1:N
-        sum_i = sum_i + gamma(j)*uInd(i,j);
-        sum_j = sum_j + gamma(j)*wInd(i,j);
+        sum = sum + gamma(j)*[uInd(i,j);wInd(i,j)];
     end
-V(i,1) = Qinf(1) + sum_i;
-V(i,2) = Qinf(2) + sum_j;
+V(i,:) = Qinf + sum;
+
 end
+
+% Velocity distribution plot
+angle = linspace(0,360,N);
+figure
+plot(angle,(V(:,1)/norm(Qinf)),'r');
+hold on
+plot(angle,(V(:,2)/norm(Qinf)),'b');
+hold off
+xlim([0,360]);
 
 % Control point pressure coefficient computation: Cp_i=1-((abs(Vi)/Qinf)^2
 Cp = 1 - (sqrt(V(:,1).^2+V(:,2).^2)/sqrt(Qinf(1,1)^2+Qinf(2,1)^2)).^2;
@@ -208,6 +239,11 @@ hold off
 % Pressure Coefficient chord distribution plot
 figure
 plot(coord_xC(:,1),Cp);
+% Pressure Coefficient angle distribution plot
+angle = linspace(0,360,N);
+figure
+plot(angle,Cp);
+xlim([0,360]);
 
 % Pressure Coefficient airfoil distribution plot
 figure
@@ -224,6 +260,9 @@ end
 plot(coord_xP(:,1),coord_xP(:,2),'k');
 axis equal
 hold off
+% Lift coefficient
+% Cl = 
+
 % Streamlines arround airfoil plot
 
 %}
