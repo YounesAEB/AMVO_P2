e64a1783250c8b5294ddfcc5fe8c6ed3ae2b1863
YounesAEB <109099577+YounesAEB@users.noreply.github.com>
2023-11-06 11:33:32 +0100

    Revert "canvis"
    This reverts commit f5f55e94f63cbb874f411e72cb2a05891a57efb3.

--- POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m
+++ POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m
@@ -6,11 +6,10 @@
 %--------------------------------------------------------------------------
 clc; clear; close all;
 
-%% EXERCISE I
 % Input parameters
-N       = 200;  % Number of panels
+N       = 128;  % Number of panels
 R       = 1;    % Radius of the cilinder
-AoA     = 6;    % Angle of attack
+AoA     = 0;    % Angle of attack
 Uinf    = 1;   % Freestream Velocity field module
 Qinf    = Uinf*[cosd(AoA);sind(AoA)]; % Freestream Velocity field
 
@@ -20,8 +19,8 @@ Qinf    = Uinf*[cosd(AoA);sind(AoA)]; % Freestream Velocity field
 [cj,sj,Ncj,Tcj] = computePanelAngleAndNormalAndTangentVectors(coord_xP,lp,N); % Panel angle, normal and tangent vectors calculation
 
 % POTENTIAL AERODYNAMICS - VELOCITY AND PRESSURE FIELDS CALCULATION
-% [gamma,uInd,wInd] = computeConstantSourceDistribution(Qinf,coord_xP,coord_xC,lp,cj,sj,Ncj,N);
-[gamma,uInd,wInd] = computeConstantVortexDistribution(Qinf,coord_xP,coord_xC,lp,cj,sj,Tcj,N);
+[gamma,uInd,wInd] = computeConstantSourceDistribution(Qinf,coord_xP,coord_xC,lp,cj,sj,Ncj,N);
+%[gamma,uInd,wInd] = computeConstantVortexDistribution(Qinf,coord_xP,coord_xC,lp,cj,sj,Tcj,N);
 
 % Preprocessing computations
 V   = computeVelocity(Qinf,gamma,uInd,wInd,N);
@@ -31,15 +30,6 @@ cp  = computeCp(Qinf,V);
 plotPanelsAndNormVectors(coord_xP,coord_xC,Ncj); % Panel and norm vector visualization 
 plotSourceStrengthDistribution(coord_xC,coord_xP,Ncj,gamma,N);
 plotVelocityDistribution(Qinf,V,N);
-plotPressureCoefficient(coord_xP,coord_xC,Ncj,cp,N)
-
-%% EXERCISE II
-isentropicExp = 1.4;
-% Entropia
-Cp_0 = min(cp);
-syms Minf2
-eqn_Cp = Cp_0/(sqrt(1-Minf2)+(Cp_0*Minf2)/(2*sqrt(1-Minf2))*(1+Minf2*(isentropicExp-1)/2)) == 2/(isentropicExp*Minf2)*(((2+(isentropicExp-1)*Minf2)/(1+isentropicExp))^(isentropicExp/(isentropicExp-1))-1);
-Mcrit2 = solve(eqn_Cp, Minf2);
-Mcrit = double(sqrt(Mcrit2(1)));
+plotPressureCoefficient(coord_xP,coord_xC,Ncj,cp,N);
 
 
--- computeCl.m
+++
@@ -1,6 +0,0 @@
-function cl = computeCl(cp,lp,Ncj,c,AoA)
-    
-    % Total lift coefficient computation: 
-    cl = -1*(sum(cp.*lp.*Ncj)/c)*[-sind(AoA);cosd(AoA)]; 
-
-end
