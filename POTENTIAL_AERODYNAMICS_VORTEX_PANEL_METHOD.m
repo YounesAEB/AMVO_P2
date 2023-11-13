e9b7cb0b98ed91ac15465ba5a478e2dea8f12434
joelrajo <100616947+joelrajo@users.noreply.github.com>
2023-11-13 12:04:52 +0100

    Changes

--- POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m
+++ POTENTIAL_AERODYNAMICS_VORTEX_PANEL_METHOD.m
@@ -11,21 +11,30 @@ set(groot,'defaultLegendInterpreter','latex');
 
 %% EXERCISE I
 % Input parameters
-N       = 512;  % Number of panels
-
 NACA    = "0010";
-% R     = 1;    % Radius of the cilinder
 c       = 1;  % Airfoil chord
-AoAaux     = [2,4,6,8];  % Angle of attack in degrees
 Uinf    = 1;   % Freestream Velocity field module
+Naux       = [16,32,64,128,256,512];  % Number of panels
+AoAaux     = [2,4,6,8,10];  % Angle of attack in degrees
 
+% Vector definition
+CL = zeros(size(Naux,2),size(AoAaux,2));
+CM4 = zeros(size(Naux,2),size(AoAaux,2));
+MCR = zeros(size(Naux,2),size(AoAaux,2));
+
+for i=1:size(Naux,2)
+% R     = 1;    % Radius of the cilinder
+N = Naux(i);
 % Precomputations
 % [coord_xP]      = setCylinderNodes(R,N); % Normally the coordinates xP are given
 [coord_xP,coord_xC,lp] = setGeometricParameters(c,N,NACA);
 [cj,sj,Ncj,Tcj] = computePanelAngleAndNormalAndTangentVectors(coord_xP,lp,N); % Panel angle, normal and tangent vectors calculation
 
-for Ncase=1:numel(AoAaux)
-    AoA = AoAaux(Ncase);
+% Vector definition
+CP = zeros(N,size(AoAaux,2));
+
+for j=1:numel(AoAaux)
+    AoA = AoAaux(j);
     Qinf = Uinf*[cosd(AoA);sind(AoA)];
 
     % POTENTIAL AERODYNAMICS - VELOCITY AND PRESSURE FIELDS CALCULATION
@@ -35,26 +44,37 @@ for Ncase=1:numel(AoAaux)
     % Preprocessing computations
     V   = computeVelocity(Qinf,gamma,uInd,wInd,N);
     cp  = computeCp(Qinf,V);
+    CP(:,j) = cp;
     cl = computeCl(cp,lp,Ncj,c,AoA);
+    CL(i,j) = cl;
     cm4 = computeCm4(cp,coord_xC,coord_xP,c);
+    CM4(i,j) = cm4; 
+
+    isentropicExp = 1.4;
+    % Laitone's Rule
+    Cp_0 = min(cp);
+    syms Minf2
+    eqn_Cp = Cp_0/(sqrt(1-Minf2)+(Cp_0*Minf2)/(2*sqrt(1-Minf2))*(1+Minf2*(isentropicExp-1)/2)) == 2/(isentropicExp*Minf2)*(((2+(isentropicExp-1)*Minf2)/(1+isentropicExp))^(isentropicExp/(isentropicExp-1))-1);
+    Mcrit2 = solve(eqn_Cp, Minf2);
+    Mcrit = double(sqrt(Mcrit2(1)));
+    MCR(i,j) = Mcrit;
 
     msg = sprintf('Cl=%i and Cm1/4=%i for AoA=%i degrees', cl, cm4, AoA);
     disp(msg);
+
 end
 % POSTPROCESSING
-plotPanelsAndNormVectors(coord_xP,coord_xC,Ncj); % Panel and norm vector visualization 
-plotSourceStrengthDistribution(coord_xC,coord_xP,Ncj,gamma,N);
-plotVelocityDistribution(Qinf,V,N);
-plotPressureCoefficient(coord_xP,coord_xC,Ncj,cp,N)
+% plotPanelsAndNormVectors(coord_xP,coord_xC,Ncj); % Panel and norm vector visualization 
+% plotSourceStrengthDistribution(coord_xC,coord_xP,Ncj,gamma,N);
+% plotVelocityDistribution(Qinf,V,N);
 
-%% EXERCISE II
-isentropicExp = 1.4;
-% Laitone's Rule
-Cp_0 = min(cp);
-syms Minf2
-eqn_Cp = Cp_0/(sqrt(1-Minf2)+(Cp_0*Minf2)/(2*sqrt(1-Minf2))*(1+Minf2*(isentropicExp-1)/2)) == 2/(isentropicExp*Minf2)*(((2+(isentropicExp-1)*Minf2)/(1+isentropicExp))^(isentropicExp/(isentropicExp-1))-1);
-Mcrit2 = solve(eqn_Cp, Minf2);
-Mcrit = double(sqrt(Mcrit2(1)));
+for a=1:size(AoAaux,2)
+plotPressureCoefficient(coord_xP,coord_xC,Ncj,CP(:,a),N)
+hold on
+end
+
+end
+
 
 
 
