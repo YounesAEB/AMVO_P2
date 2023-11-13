f8b1732a5c4db219a93ddea86b9686baad041703
joelrajo <100616947+joelrajo@users.noreply.github.com>
2023-11-13 12:18:10 +0100

    Revert "Merge branch 'JOEL' of https://github.com/YounesAEB/AMVO_P2 into JOEL"
    This reverts commit e027d2d7797e42c9b5ee1dbd1d0d9798d575d7d9, reversing
    changes made to e9b7cb0b98ed91ac15465ba5a478e2dea8f12434.

--- POTENTIAL_AERODYNAMICS_CRITICAL_MACH.m
+++ POTENTIAL_AERODYNAMICS_CRITICAL_MACH.m
@@ -5,25 +5,37 @@
 %  /  MUEA - MQ1 - Younes Akhazzan - Joel Rajo - Pol Ruiz                         
 %--------------------------------------------------------------------------
 clc; clear; %close all;
+set(groot,'defaultAxesTickLabelInterpreter','latex');  
+set(groot,'defaulttextinterpreter','latex');
+set(groot,'defaultLegendInterpreter','latex');
 
 % Input parameters
-N             = 200;  % Number of panels
-R             = 1;    % Radius of the cilinder
-c             = 2*R;  % Equivalent chord
-AoA           = 6;    % Angle of attack
+N             = 512;  % Number of panels
+NACA          = "0010";
+% R             = 1;    % Radius of the cilinder
+c             = 1;  % Equivalent chord
+AoA           = 2;    % Angle of attack
 isentropicExp = 1.4; % Gamma ideal gas
-Mcrit         = 0.336947168481477;
+Mcrit         = 0.604052986849087;
 a             = 340;  % [m/s] Sound velocity
-M             = linspace(0.000001,Mcrit,25);
+M             = [Mcrit-0.15,Mcrit-0.1,Mcrit-0.05,Mcrit];
+
+% Vector definition
 clCorrected   = zeros(size(M,2),1);
+
 for i=1:size(M,2)
 Uinf    = M(1,i)*a;   % Freestream Velocity field module
 B       = sqrt(1-M(1,i)^2); %Prandlt-Glauert Correction Parameter
 Qinf    = Uinf*[cosd(AoA);sind(AoA)]; % Freestream Velocity field
 
 % Precomputations
-[coord_xP]      = setCylinderNodes(R,N); % Normally the coordinates xP are given
-[coord_xC,lp]   = setGeometricParameters(coord_xP,N);
+
+% Cilinder
+% [coord_xP]      = setCylinderNodes(R,N); % Normally the coordinates xP are given
+% [coord_xC,lp]   = setGeometricParameters(coord_xP,N);
+
+% Airfoil
+[coord_xP,coord_xC,lp] = setGeometricParameters(c,N,NACA);
 [cj,sj,Ncj,Tcj] = computePanelAngleAndNormalAndTangentVectors(coord_xP,lp,N); % Panel angle, normal and tangent vectors calculation
 
 % POTENTIAL AERODYNAMICS - VELOCITY AND PRESSURE FIELDS CALCULATION
@@ -36,12 +48,6 @@ cp  = computeCp(Qinf,V);
 cpCorrected = cp./(sqrt(1-M(1,i)^2)+(cp.*M(1,i)^2)/(2*sqrt(1-M(1,i)^2))*(1+M(1,i)^2*(isentropicExp-1)/2));
 cl = computeCl(cpCorrected,lp,Ncj,c,AoA);
 clCorrected(i) = cl;
-
-% POSTPROCESSING
-% plotPanelsAndNormVectors(coord_xP,coord_xC,Ncj); % Panel and norm vector visualization 
-% plotSourceStrengthDistribution(coord_xC,coord_xP,Ncj,gamma,N);
-% plotVelocityDistribution(Qinf,V,N);
-% plotPressureCoefficient(coord_xP,coord_xC,Ncj,cp,N)
 
 end
 
